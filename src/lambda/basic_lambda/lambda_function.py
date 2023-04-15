7eac1db899ffc6e878ae4ce1be3236c9c5015621
09371116 <mete.capar@ext.kocdigital.com>
2023-04-15 23:55:47 +0300

    read csv handler

--- src/lambda/basic_lambda/lambda_function.py
+++ src/lambda/basic_lambda/lambda_function.py
@@ -2,14 +2,17 @@ import datetime
 from io import StringIO
 import json
 import boto3
-import logging,time
-import csv,pika
+import logging
+import time
+import csv
+import pika
 import paho.mqtt.client as mqtt
 from collections import defaultdict
 
 messageQueueType = 'sqs'
 
-def amqpSender(status,message):
+
+def amqpSender(status, message):
     user = ""
     password = ""
     host = ""
@@ -17,18 +20,22 @@ def amqpSender(status,message):
     virtual_host = ""
 
     # Connect to RabbitMQ
-    credentials = pika.PlainCredentials(user,password)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host,port,virtual_host,credentials))
+    credentials = pika.PlainCredentials(user, password)
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host, port, virtual_host, credentials))
 
     channel = connection.channel()
 
     # Declare a queue
     channel.queue_declare(queue='data_queue')
     if status == 1:
-        channel.basic_publish(exchange='', routing_key='data_queue', body=json.dumps(message))
+        channel.basic_publish(
+            exchange='', routing_key='data_queue', body=json.dumps(message))
     else:
-        channel.basic_publish(exchange='', routing_key='data_queue_error', body=json.dumps(message))
-        
+        channel.basic_publish(
+            exchange='', routing_key='data_queue_error', body=json.dumps(message))
+
+
 def mqttSender(status, record):
     # Define MQTT settings
     MQTT_BROKER_HOST = "mqtt.example.com"
@@ -42,7 +49,7 @@ def mqttSender(status, record):
     # Create MQTT client instance
     mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
     mqtt_client.username_pw_set(MQTT_USER, MQTT_PASS)
-    mqtt_client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)    
+    mqtt_client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
     if status == 1:
         mqtt_client.publish(MQTT_TOPIC_CUSTOMER_MESSAGES, json.dumps(record))
         time.sleep(0.1)
@@ -50,18 +57,19 @@ def mqttSender(status, record):
         mqtt_client.publish(MQTT_TOPIC_ERROR_MESSAGES, json.dumps(record))
         time.sleep(0.1)
 
+
 def sqsSender(status, messageSQS):
     # Publish results to Amazon SQS
     sqs = boto3.resource('sqs')
     queue_url = 'https://sqs.us-west-1.amazonaws.com/813334080301/sqs-mete-meteparserqueueE077929E-IrKAPUgO1Kvb'
-    
+
     if status == 1:
         sqs_message = {
             'Id': str(messageSQS['customer_reference']),
             'MessageBody': json.dumps(messageSQS)
         }
         sqs_response = sqs.meta.client.send_message(
-            QueueUrl=queue_url, MessageBody=json.dumps(sqs_message))      
+            QueueUrl=queue_url, MessageBody=json.dumps(sqs_message))
     else:
         sqs_message = {
             'Id': str(messageSQS['order_reference']),
@@ -69,12 +77,16 @@ def sqsSender(status, messageSQS):
         }
         sqs_response = sqs.meta.client.send_message(
             QueueUrl=queue_url, MessageBody=json.dumps(sqs_message))
-    
+
+
 def read_csv_from_s3(s3, bucket, key):
-    response = s3.get_object(Bucket=bucket, Key=key)
-    content = StringIO(response['Body'].read().decode('utf-8'))
-    reader = csv.DictReader(content)
-    return [row for row in reader]
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        content = StringIO(response['Body'].read().decode('utf-8'))
+        reader = csv.DictReader(content)
+        return [row for row in reader]
+    except:
+        logger.error("Error while reading from s3")
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -93,12 +105,10 @@ def lambda_handler(event, context):
 
     s3 = boto3.client('s3')
     bucketName = 'mete-bucket-55'
-    try:
-        customers = read_csv_from_s3(s3, bucketName, customer_file)
-        orders = read_csv_from_s3(s3, bucketName, orders_file)
-        items = read_csv_from_s3(s3, bucketName, items_file)
-    except:
-        logger.error("Error while reading from s3")
+    customers = read_csv_from_s3(s3, bucketName, customer_file)
+    orders = read_csv_from_s3(s3, bucketName, orders_file)
+    items = read_csv_from_s3(s3, bucketName, items_file)
+        
     customer_dict = {c["customer_reference"]: c for c in customers}
     order_dict = {o["order_reference"]: o for o in orders}
     order_item_dict = defaultdict(list)
@@ -137,7 +147,8 @@ def lambda_handler(event, context):
         if customer_ref in customer_dict:
             customer_summary[customer_ref]['orders'] += 1
             for item in order_item_dict[order["order_reference"]]:
-                customer_summary[customer_ref]['total_price'] += float(item["total_price"])
+                customer_summary[customer_ref]['total_price'] += float(
+                    item["total_price"])
 
     customer_messages = [{
         "type": "customer_message",
@@ -152,19 +163,19 @@ def lambda_handler(event, context):
     # Send customer messages
     for message in customer_messages:
         if messageQueueType == 'sqs':
-            sqsSender(1,message)
+            sqsSender(1, message)
         elif messageQueueType == 'amqp':
-            amqpSender(1,message)
+            amqpSender(1, message)
         elif messageQueueType == 'mqtt':
-            mqttSender(1,message)
+            mqttSender(1, message)
     # Send error messages
     for message in all_error_messages:
         if messageQueueType == 'sqs':
-            sqsSender(0,message)
+            sqsSender(0, message)
         elif messageQueueType == 'amqp':
-            amqpSender(0,message)
+            amqpSender(0, message)
         elif messageQueueType == 'mqtt':
-            mqttSender(0,message)
+            mqttSender(0, message)
 
     return {
         "statusCode": 200,
